ced39721d00d289a89b5f11cfac6caa386d68fdd
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-08 21:49:53 +0300

    Add files via upload

---
+++ Лысков блок 1 задача 1.pas
@@ -0,0 +1,9 @@
+﻿program one;
+begin
+  var s, r, pi: real;
+  write ('Введите радиус круга: ');
+  read (r);
+  pi:=3.14;
+  s:= pi*r*r;
+  write ('Площадь круга', ' = ',s);
+end.
---
+++ Лысков блок 1 задача 2.pas
@@ -0,0 +1,10 @@
+﻿program two;
+var l, s, pi, r: real;
+begin
+  write ('Введите длину окружности: ');
+  read (l);
+  pi:= 3.1415926535;
+  r:=l/(2*pi);
+  s:=(l*r)/2;
+  write ('Площадь окружности = ', s);
+end.
