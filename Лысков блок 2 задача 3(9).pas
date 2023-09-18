e3a3b4df120b57e55c0889b67d11750b1b3396ba
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-18 18:36:16 +0300

    Add files via upload

---
+++ Лысков блок 2 задача 3(9).pas
@@ -0,0 +1,10 @@
+﻿program trrr;
+var a, b, c, dop: integer;
+begin
+  write ('Введите трехзначное число через пробел: ');
+  read (a,b,c);
+  dop:=a;
+  a:=c;
+  c:=dop;
+  write (a,b,c);
+end.
---
+++ Лысков блок 3 задача 2.py
@@ -0,0 +1,10 @@
+print('Enter three numbers: ')
+a = int(input())
+b = int(input())
+c = int(input())
+mn = min(a, b, c)
+mx = max(a, b, c)
+sred = a + b + c - mn - mx
+print(mn)
+print(sred)
+print(mx)
