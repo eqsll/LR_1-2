59fe3d6603f1a0894771aafa2fdd4dfc6cf5ed11
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-21 13:05:45 +0300

    Add files via upload

---
+++ Лысков блок 4 задача 1.pas
@@ -0,0 +1,13 @@
+﻿program brrr;
+var x, a, b, c, d, i: integer;
+begin
+  write ('Введите четырехзначное число: ');
+  read (x);
+  a:= x div 1000;
+  b:= x div 100 mod 10;
+  c:= x div 10 mod 10;
+  d:= x mod 10;
+  if (a=d) and (b=c) then 
+    write ('Число является палиндромом')
+    else write ('Число не является палиндромом');
+end.
