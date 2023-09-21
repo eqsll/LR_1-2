e0424a4e7c6d2470b7486c1d80b1cecc83b0b666
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-21 12:04:01 +0300

    Add files via upload

---
+++ Лысков блок 3 задача 3.pas
@@ -0,0 +1,10 @@
+﻿program hshs;
+var a, b, c, pol: integer;
+begin
+  writeln ('Введите три числа: ');
+  readln (a, b, c);
+  if a > 0 then inc (pol);
+  if b > 0 then inc (pol);
+  if c > 0 then inc (pol);
+  write ('Количество положительных чисел: ', pol);
+end.
---
+++ Лысков блок 3 задача 5.pas
@@ -0,0 +1,15 @@
+﻿program korn;
+var a, b, c, D, x1, x2: real;
+begin
+  write ('Введиете коэффициент a: ');
+  read (a);
+  write ('Введиете коэффициент b: ');
+  read (b);
+  write ('Введиете коэффициент c: ');
+  read (c);
+  D:= b**2 - 4*a*c;
+  x1:= (-b + sqrt(D))/(2*a);
+  x2:= (-b - sqrt(D))/(2*a);
+  writeln ('x1 = ',x1);
+  writeln ('x2 = ',x2);
+end.
