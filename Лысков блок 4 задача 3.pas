7313ccd9fce52fec072b83f62f8a550b2b355940
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 15:04:35 +0300

    Add files via upload

---
+++ Лысков блок 4 задача 3.pas
@@ -0,0 +1,23 @@
+﻿program cxsg;
+var a, b, c: integer;
+begin
+  write ('Введите число "a" : ');
+  readln (a);
+  write ('Введите число "b" : ');
+  readln (b);
+  write ('Введите число "c" : ');
+  readln (c);
+  begin
+    if a mod 2 = 0 then
+      writeln ('Число "a" четное')
+    else writeln ('Число "а" нечетное');
+    
+    if b mod 2 = 0 then
+      writeln ('Число "b" четное')
+    else writeln ('Число "b" нечетное');
+    
+    if c mod 2 = 0 then
+      writeln ('Число "c" четное')
+    else writeln ('Число "c" нечетное');
+  end;
+end.
