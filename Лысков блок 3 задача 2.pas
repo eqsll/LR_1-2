f55ac80f6991d0d604515492b5537503ad8135e5
eqsll <144461926+eqsll@users.noreply.github.com>
2023-10-09 11:16:04 +0300

    Add files via upload

---
+++ Лысков блок 3 задача 2.pas
@@ -0,0 +1,9 @@
+﻿var a, b, c, mn, mx, sred: integer;
+begin
+  write ('Введите три числа: ');
+  read (a, b, c);
+  mn:= min (a, b, c);
+  mx:= max (a, b, c);
+  sred:= a + b + c - mn - mx;
+  writeln (mn,' ', sred,' ', mx);
+end.
