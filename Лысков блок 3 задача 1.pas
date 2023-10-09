95b94bfd12916082257a055dd37ed60445ae307f
eqsll <144461926+eqsll@users.noreply.github.com>
2023-10-09 11:04:52 +0300

    Add files via upload

---
+++ Лысков блок 3 задача 1.pas
@@ -0,0 +1,12 @@
+﻿var a, b, c: real;
+begin
+  write ('Введите три числа: ');
+  read (a, b, c);
+  if (b >= a) and (a <= c) then
+    write ('Наименьшее число: ', a)
+  else
+    if (a >= b) and (b <= c) then
+    write ('Наименьшее число: ', b)
+  else
+    write ('Наименьшее число: ', c);
+end.
